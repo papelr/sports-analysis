78549f8285092a90c8f38f3ecd826ab483f0cbec
papelr <robertpapel@gmail.com>
2018-07-13 15:17:45 -0500

    WHIP R file

---
+++ MLB/mlb-whip-analysis.R
@@ -0,0 +1,11 @@
+#' ---------
+#' Title: How WHIP Changes with Age
+#' Subtitle: MLB - WHIP Stat
+#' Date: July 2018
+#' Author: Robert Papel
+#' ---------
+
+#'###### -------------**Libraries**---------------------- ######
+
+library(tidyverse)
+library(rvest)
--- nhl-war-analysis.R
+++
@@ -1,7 +0,0 @@
-#' ---------
-#' Title: How WAR Changes with Age
-#' Subtitle: NHL - Wins Above Replacement
-#' Date: July 2018
-#' Author: Robert Papel
-#' ---------
-
