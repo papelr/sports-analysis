2152a38c6352db323131233277c5340e6476f754
papelr <robertpapel@gmail.com>
2018-07-14 11:48:16 -0500

    Need to find the stat, and need to write a funtion to scrape...

--- NHL/nhl-war-analysis.R
+++ NHL/nhl-war-analysis.R
@@ -5,3 +5,25 @@
 #' Author: Robert Papel
 #' ---------
 
+#'###### -------------**Libraries**---------------------- ######
+
+library(tidyverse)
+library(rvest)
+
+#'###### -------------**Scrape**---------------------- ######
+
+fil_link <- "https://www.hockey-reference.com/players/f/forsbfi01.html"
+
+fil_stats <- NULL
+
+for (i in 1:length(fil_link)) {
+  
+  fil_standard <- fil_link[i] %>%
+    read_html %>%
+    html_node("#stats_basic_plus_nhl") %>%
+    html_table(header = T)
+  
+  fil_stats <- rbind(fil_standard, fil_pos)
+  
+}
+
