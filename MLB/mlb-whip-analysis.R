aa7fa5ec28beed7b7c670aab1a53b2d5b3cb0bd4
papelr <robertpapel@gmail.com>
2018-07-14 13:18:05 -0500

    got rid of ESPN's repeated header rows, finally

--- MLB/mlb-whip-analysis.R
+++ MLB/mlb-whip-analysis.R
@@ -13,7 +13,7 @@ library(rvest)
 #'###### -------------**Scrape**---------------------- ######
 
 # A function that creates a "year" parameter within the ESPN links
-get_whip_data <- function(season) {
+espn_pitching_stats <- function(season) {
   
   # Creating the URL vectors, without the year
   whip_a <- paste0("http://www.espn.com/mlb/stats/pitching/_/year/", season, 
@@ -40,22 +40,25 @@ get_whip_data <- function(season) {
     html_table(header = T)
   
   # Binding all the tables together
-  yr_df <- rbind(a_data, b_data, c_data)   
-  yr_df$year <- yr_param
+  pitching_tables <- rbind(a_data, b_data, c_data)   
+  pitching_tables$Year <- season
   
-  return(yr_df)
+  return(pitching_tables)
 }
 
-df_list <- lapply(2005:2017, get_whip_data)
+# Uses the function to pull in 2000-201_ seasons
+whip_table <- lapply(2000:2017, espn_pitching_stats)
+final_table <- do.call(rbind, whip_table)  
 
-final_df <- do.call(rbind, df_list)  
+# Adjusting column headers
+names(final_table) <- as.matrix(final_table[1, ])
+final_table <- final_table[-1, ]
 
-# Adjusting rows
-names(whip_2018) <- as.matrix(whip_2018[1, ])
-whip_2018 <- whip_2018[-1, ]
-whip_2018 <- whip_2018[c(-11,-22,-33,-44,-55,-66,-77,-88), ]
-whip_2018$"YEAR" <- 2018 
-whip_2018[] <- lapply(whip_2018, function(x) type.convert(as.character(x)))
+# Getting rid of inner headers - the RK PLAYER TEAM, etc.
+final_table <- final_table[final_table$RK != "RK", ]
+final_table[] <- lapply(final_table, function(x) type.convert(as.character(x), 
+                                                      as.is = TRUE))
 
+# Selecting specific variables
 whip_2018 <- whip_2018 %>% 
   select(YEAR, PLAYER, TEAM, GP, IP, SO, W, L, WAR, WHIP, ERA)
