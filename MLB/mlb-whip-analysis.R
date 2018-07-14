69ff4b3cf022f1c4cfa3f506157bc922ce2fdb68
papelr <robertpapel@gmail.com>
2018-07-14 11:52:04 -0500

    function created, with major help from stack overflow!!!

--- MLB/mlb-whip-analysis.R
+++ MLB/mlb-whip-analysis.R
@@ -1,6 +1,6 @@
 #' ---------
-#' Title: How whip Changes with Age
-#' Subtitle: MLB - whip Stat
+#' Title: How WHIP Changes with Age
+#' Subtitle: MLB - WHIP Stat
 #' Date: July 2018
 #' Author: Robert Papel
 #' ---------
@@ -12,44 +12,43 @@ library(rvest)
 
 #'###### -------------**Scrape**---------------------- ######
 
-# Links for pitching stats, includes WHIP statistic
-whip_2018a <- "http://www.espn.com/mlb/stats/pitching/_/order/false"
-whip_2018b <- "http://www.espn.com/mlb/stats/pitching/_/count/41/qualified/true/order/false"
-whip_2018c <- "http://www.espn.com/mlb/stats/pitching/_/count/81/qualified/true/order/false"
-
-whip_2017a <- "http://www.espn.com/mlb/stats/pitching/_/year/2017/order/false"
-whip_2017b <- "http://www.espn.com/mlb/stats/pitching/_/year/2017/count/41/qualified/true/order/false")
-
-whip_2016 <- "http://www.espn.com/mlb/stats/pitching/_/year/2016/order/false"
-whip_2015 <- "http://www.espn.com/mlb/stats/pitching/_/year/2015/order/false"
-whip_2014 <- "http://www.espn.com/mlb/stats/pitching/_/year/2014/order/false"
-whip_2013 <- "http://www.espn.com/mlb/stats/pitching/_/year/2013/order/false"
-whip_2012 <- "http://www.espn.com/mlb/stats/pitching/_/year/2012/order/false"
-whip_2011 <- "http://www.espn.com/mlb/stats/pitching/_/year/2011/order/false"
-whip_2010 <- "http://www.espn.com/mlb/stats/pitching/_/year/2010/order/false"
-whip_2009 <- "http://www.espn.com/mlb/stats/pitching/_/year/2009/order/false"
-whip_2008 <- "http://www.espn.com/mlb/stats/pitching/_/year/2008/order/false"
-whip_2007 <- "http://www.espn.com/mlb/stats/pitching/_/year/2007/order/false"
-whip_2006 <- "http://www.espn.com/mlb/stats/pitching/_/year/2006/order/false"
-whip_2005 <- "http://www.espn.com/mlb/stats/pitching/_/year/2005/order/false"
-               
-# Scraping 2018 tables - there are multiple pages to the list
-a2018 <- whip_2018a %>%
-  read_html %>%
-  html_node("#my-players-table > div > div.mod-content > table") %>%
-  html_table(header = T)
+# A function that creates a "year" parameter within the ESPN links
+get_whip_data <- function(season) {
   
-b2018 <- whip_2018b %>%
-  read_html %>%
-  html_node("#my-players-table > div > div.mod-content > table") %>%
-  html_table(header = T)
+  # Creating the URL vectors, without the year
+  whip_a <- paste0("http://www.espn.com/mlb/stats/pitching/_/year/", season, 
+                   "/order/false")
+  whip_b <- paste0("http://www.espn.com/mlb/stats/pitching/_/year/", season, 
+                   "/count/41/qualified/true/order/false")
+  whip_c <- paste0("http://www.espn.com/mlb/stats/pitching/_/year/", season, 
+                   "/count/81/order/false")
   
-c2018 <- whip_2018c %>%
-  read_html %>%
-  html_node("#my-players-table > div > div.mod-content > table") %>%
-  html_table(header = T)
+  # Building the data tables, ignores the 3rd if only two pages in table
+  a_data <- whip_a %>%
+    read_html %>%
+    html_node("#my-players-table > div > div.mod-content > table") %>%
+    html_table(header = T)
+  
+  b_data <- whip_b %>%
+    read_html %>%
+    html_node("#my-players-table > div > div.mod-content > table") %>%
+    html_table(header = T)
+  
+  c_data <- whip_c %>%
+    read_html %>%
+    html_node("#my-players-table > div > div.mod-content > table") %>%
+    html_table(header = T)
+  
+  # Binding all the tables together
+  yr_df <- rbind(a_data, b_data, c_data)   
+  yr_df$year <- yr_param
+  
+  return(yr_df)
+}
 
-whip_2018 <- rbind(a2018, b2018, c2018)
+df_list <- lapply(2005:2017, get_whip_data)
+
+final_df <- do.call(rbind, df_list)  
 
 # Adjusting rows
 names(whip_2018) <- as.matrix(whip_2018[1, ])
@@ -60,23 +59,3 @@ whip_2018[] <- lapply(whip_2018, function(x) type.convert(as.character(x)))
 
 whip_2018 <- whip_2018 %>% 
   select(YEAR, PLAYER, TEAM, GP, IP, SO, W, L, WAR, WHIP, ERA)
-
-
-
-
-#'###### -------------**Detritus**---------------------- ######
-
-"http://www.espn.com/mlb/stats/pitching/_/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2017/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2016/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2015/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2014/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2013/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2012/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2011/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2010/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2009/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2008/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2007/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2006/order/false",
-"http://www.espn.com/mlb/stats/pitching/_/year/2005/order/false"
