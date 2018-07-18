29ed647fa72247e446195dde64cbcf83bb87b9b1
papelr <robertpapel@gmail.com>
2018-07-18 18:13:46 -0500

    going to create team scrape funtion, like the pitcher file

---
+++ MLB/mlb-team-stat-functions.R
@@ -0,0 +1,10 @@
+#' ---------
+#' Title: MLB Team Statistics 
+#' Subtitle: Functions for Scraping and Visualizing the non-Pitchers
+#' Date: July 2018
+#' Author: Robert Papel
+#' ---------
+
+#'###### ---------------------------------------------------- ######
+#'###### -------------**Scrape Function**-------------------- ######
+#'###### ---------------------------------------------------- ######
