73383d8955225713304364a23e380958070129aa
papelr <robertpapel@gmail.com>
2018-07-19 18:05:16 -0500

    able to save a table from the 2018 season

--- NHL/nhl-corsica-scrape.R
+++ NHL/nhl-corsica-scrape.R
@@ -8,17 +8,15 @@
 #'###### -------------**Libraries**---------------------- ######
 
 library(tidyverse)
-library(rvest)
+library(readr)
+library(clipr)
+
+#'###### -------------**CSV Copy & Save**---------------------- ######
+
+# Corsica lets one copy/paste csv data: read and saved in wd() here:
+nhl <- read.csv(text = clipr::read_clip())
+write_csv(nhl, "corsica-all-player-data.csv") 
 
 #'###### -------------**Scrape**---------------------- ######
 
-# Data from Corsica
-corsica_url <- "https://www.corsicahockey.com/nhl/players/nhl-player-stats"
-
-Right  <- corsica_url %>%
-  read_html %>%
-  html_node("#post-116 > div > div.actions-header > div.player-stats-table-wrap.table-scroll.active > div > table:nth-child(1)") %>%
-  html_table(header = T) 
-                         
-                         
                         
