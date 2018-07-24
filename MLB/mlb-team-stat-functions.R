f23c4b1eba4eeb442097f94294e098eb0f6c3620
papelr <robertpapel@gmail.com>
2018-07-24 14:16:13 -0500

    working scrape, need to combine that function and vis function

--- MLB/mlb-team-stat-functions.R
+++ MLB/mlb-team-stat-functions.R
@@ -9,46 +9,36 @@
 #'###### -------------**Scrape Function**-------------------- ######
 #'###### ---------------------------------------------------- ######
 
-# [UNDER CONSTRUCTION]
-
 # Run this function before doing the vis function!
 
 # Function to scrape MLB team statistics
-espn_team_stats <- function(side, team, season) {
+espn_team_scrape <- function(team, side, season) {
   
   # Libraries
   library(tidyverse)
   library(rvest)
-
-  # URL vector
-  side_url <- paste0("http://www.espn.com/mlb/team/stats/", side 
   
-  team_url <- paste0("http://www.espn.com/mlb/team/stats/", side, "/_/name/",
-                     team
-
-  season_url <- paste0("http://www.espn.com/mlb/team/stats/", side, "/_/name/",
-                        team, "/year/", season, "/")
-
+  # URL, pasted together
+  team_url <- paste0("http://www.espn.com/mlb/team/stats/", 
+                     side, "/_/name/", 
+                     team, "/year/", 
+                     season, "/")
+  
   # Building the data table
-  team_page <- team_url %>%
+  team_tables <- team_url %>%
     read_html %>%
     html_node("#my-players-table > div.mod-container.mod-table > 
               div.mod-content > table:nth-child(1)") %>%
     html_table(header = T)
   
-  team_tables <- team_page
-  team_tables$Year <- c(team, season, side)
+  team_tables$Year <- season
   
   return(team_tables)
-  
 }
 
-# Uses the function to pull in 2000-2018 seasons
-mappppp <- Map(espn_team_stats, side_url, team_url, season_url)
-
-applying <- lapply(2000:2018, espn_team_stats)
-final_team_stats <- do.call(rbind, applying)  
 
 
-# Map: 
-df <- expand.grid(side=c("batting", "fielding"), team=c("team1", "team2"), season=c(2015, 2016, 2017)); mappppp <- Map(espn_team_stats, df$side, df$team, df$season)
+
+
+
+
